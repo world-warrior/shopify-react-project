Shopify/shopify-frontend-template-react

Re-authenticate when online token expires

hooks/useShopifyMutation.js
@@ -1,20 +1,41 @@
 import { getSessionToken } from '@shopify/app-bridge-utils'
 import { useAppBridge } from '@shopify/app-bridge-react'
+import { Redirect } from '@shopify/app-bridge/actions'
 import { useMutation } from 'react-query'
-import { request } from 'graphql-request'
+import { rawRequest } from 'graphql-request'
 
 export const useShopifyMutation = (query) => {
   const app = useAppBridge()
 
-  const {mutateAsync, ...mutationProps} = useMutation(async (variables) => {
-    const sessionToken = await getSessionToken(app)
-    const headers = new Headers({})
+  const {mutateAsync, ...mutationProps} = useMutation(
+    async (variables) => {
+      const sessionToken = await getSessionToken(app)
+      const headers = new Headers({})
 
-    headers.append('Authorization', `Bearer ${sessionToken}`)
-    headers.append('X-Requested-With', 'XMLHttpRequest')
-    const response = await request('/api/graphql', query, variables, headers)
-    return response
-  })
+      headers.append('Authorization', `Bearer ${sessionToken}`)
+      headers.append('X-Requested-With', 'XMLHttpRequest')
+      const response = await rawRequest('/api/graphql', query, variables, headers)
+      checkHeaders(response.headers, app)
+
+      return response
+    }, 
+    {
+      onError: (result) => {
+        const {response} = result
+        checkHeaders(response.headers, app)
+      },
+    },
+  )
 
   return [mutateAsync, mutationProps]
+}
+
+
+const checkHeaders = (headers, app) => {
+  if (headers.get('X-Shopify-API-Request-Failure-Reauthorize') === '1') {
+    const authUrlHeader = headers.get('X-Shopify-API-Request-Failure-Reauthorize-Url')
+
+    const redirect = Redirect.create(app)
+    redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/api/auth`)
+  }
 }

hooks/useShopifyQuery.js
@@ -1,18 +1,39 @@
 import { getSessionToken } from '@shopify/app-bridge-utils'
 import { useAppBridge } from '@shopify/app-bridge-react'
+import { Redirect } from '@shopify/app-bridge/actions'
 import { useQuery } from 'react-query'
-import { request } from 'graphql-request'
+import { rawRequest } from 'graphql-request'
 
 export const useShopifyQuery = (key, query) => {
   const app = useAppBridge()
 
-  return useQuery(key, async (variables) => {
-    const sessionToken = await getSessionToken(app)
-    const headers = new Headers({})
+  return useQuery(
+    key, 
+    async (variables) => {
+      const sessionToken = await getSessionToken(app)
+      const headers = new Headers({})
 
-    headers.append('Authorization', `Bearer ${sessionToken}`)
-    headers.append('X-Requested-With', 'XMLHttpRequest')
-    const response = await request('/api/graphql', query, variables, headers)
-    return response
-  })
+      headers.append('Authorization', `Bearer ${sessionToken}`)
+      headers.append('X-Requested-With', 'XMLHttpRequest')
+      const response = await rawRequest('/api/graphql', query, variables, headers)
+      checkHeaders(response.headers, app)
+
+      return response
+    },
+    {
+      onError: (result) => {
+        const {response} = result
+        checkHeaders(response.headers, app)
+      },
+    }
+  )
+}
+
+const checkHeaders = (headers, app) => {
+  if (headers.get('X-Shopify-API-Request-Failure-Reauthorize') === '1') {
+    const authUrlHeader = headers.get('X-Shopify-API-Request-Failure-Reauthorize-Url')
+
+    const redirect = Redirect.create(app)
+    redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/api/auth`)
+  }
 }

world-warrior/shopify-react-project

hooks/useShopifyMutation.js
@@ -1,20 +1,41 @@
 import { getSessionToken } from '@shopify/app-bridge-utils'
 import { useAppBridge } from '@shopify/app-bridge-react'
+import { Redirect } from '@shopify/app-bridge/actions'
 import { useMutation } from 'react-query'
-import { request } from 'graphql-request'
+import { rawRequest } from 'graphql-request'
 
 export const useShopifyMutation = (query) => {
   const app = useAppBridge()
 
-  const {mutateAsync, ...mutationProps} = useMutation(async (variables) => {
-    const sessionToken = await getSessionToken(app)
-    const headers = new Headers({})
+  const {mutateAsync, ...mutationProps} = useMutation(
+    async (variables) => {
+      const sessionToken = await getSessionToken(app)
+      const headers = new Headers({})
 
-    headers.append('Authorization', `Bearer ${sessionToken}`)
-    headers.append('X-Requested-With', 'XMLHttpRequest')
-    const response = await request('/api/graphql', query, variables, headers)
-    return response
-  })
+      headers.append('Authorization', `Bearer ${sessionToken}`)
+      headers.append('X-Requested-With', 'XMLHttpRequest')
+      const response = await rawRequest('/api/graphql', query, variables, headers)
+      checkHeadersForReauthorization(response.headers, app)
+
+      return response
+    }, 
+    {
+      onError: (result) => {
+        const {response} = result
+        checkHeadersForReauthorization(response.headers, app)
+      },
+    },
+  )
 
   return [mutateAsync, mutationProps]
+}
+
+
+const checkHeadersForReauthorization = (headers, app) => {
+  if (headers.get('X-Shopify-API-Request-Failure-Reauthorize') === '1') {
+    const authUrlHeader = headers.get('X-Shopify-API-Request-Failure-Reauthorize-Url')
+
+    const redirect = Redirect.create(app)
+    redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/api/auth`)
+  }
 }

hooks/useShopifyQuery.js
@@ -1,18 +1,39 @@
 import { getSessionToken } from '@shopify/app-bridge-utils'
 import { useAppBridge } from '@shopify/app-bridge-react'
+import { Redirect } from '@shopify/app-bridge/actions'
 import { useQuery } from 'react-query'
-import { request } from 'graphql-request'
+import { rawRequest } from 'graphql-request'
 
 export const useShopifyQuery = (key, query) => {
   const app = useAppBridge()
 
-  return useQuery(key, async (variables) => {
-    const sessionToken = await getSessionToken(app)
-    const headers = new Headers({})
+  return useQuery(
+    key, 
+    async (variables) => {
+      const sessionToken = await getSessionToken(app)
+      const headers = new Headers({})
 
-    headers.append('Authorization', `Bearer ${sessionToken}`)
-    headers.append('X-Requested-With', 'XMLHttpRequest')
-    const response = await request('/api/graphql', query, variables, headers)
-    return response
-  })
+      headers.append('Authorization', `Bearer ${sessionToken}`)
+      headers.append('X-Requested-With', 'XMLHttpRequest')
+      const response = await rawRequest('/api/graphql', query, variables, headers)
+      checkHeadersForReauthorization(response.headers, app)
+
+      return response
+    },
+    {
+      onError: (result) => {
+        const {response} = result
+        checkHeadersForReauthorization(response.headers, app)
+      },
+    }
+  )
+}
+
+const checkHeadersForReauthorization = (headers, app) => {
+  if (headers.get('X-Shopify-API-Request-Failure-Reauthorize') === '1') {
+    const authUrlHeader = headers.get('X-Shopify-API-Request-Failure-Reauthorize-Url')
+
+    const redirect = Redirect.create(app)
+    redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/api/auth`)
+  }
 }